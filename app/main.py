d54c4e217e5e47440d68835ab813eb1130438dc8
im <poson_hemp@yahoo.co.jp>
2023-04-10 06:20:56 +0900

    add/ GET query parameter treat

--- app/main.py
+++ app/main.py
@@ -3,13 +3,11 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
-    user_id: int
-
 @app.get('/hello/{user_id}')
-async def get_hello_world_test(user_id: int):
+async def get_hello_world_test(user_id: int, q: str = ""):
     """Get handler: for test"""
     return {
         "user_id": user_id,
+        "query": q,
         "message": "Hello, world!"
     }
