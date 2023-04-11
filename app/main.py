a0fa0385593b0e364078b971227e43717f57a8cf
im <poson_hemp@yahoo.co.jp>
2023-04-11 09:29:39 +0900

    add/ post_user_dummy

--- app/main.py
+++ app/main.py
@@ -3,8 +3,23 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class UserDummy(BaseModel):
+    name: str
+    lang: str
+
+
+@app.post('/user_dummy/')
+def post_user_dummy(user: UserDummy):
+    """POST handler: for req and res test"""
+    return {
+        "name": user.name,
+        "lang": user.lang
+    }
+
+
 @app.get('/hello/{user_id}')
-async def get_hello_world_test(user_id: int, q: str = ""):
+def get_hello_world(user_id: int, q: str = ""):
     """Get handler: for test"""
     return {
         "user_id": user_id,
